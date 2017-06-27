5e937d340a6d9c48da82b527a721d2ec5fee81fe
Niklas Klein <mail@taig.io>
2017-06-27 03:17:44 +0200

    Test Translation#& operator

--- shared/src/test/scala/io/taig/lokal/LocalizationTest.scala
+++ shared/src/test/scala/io/taig/lokal/LocalizationTest.scala
@@ -1,8 +1,16 @@
 package io.taig.lokal
 
+import cats.data.NonEmptyList
 import org.scalatest.{ FlatSpec, Matchers }
 
 class LocalizationTest extends FlatSpec with Matchers {
+    it should "allow to append a Localization" in {
+        val de = Localization( Identifier.de, "Hallo" )
+        val de2 = Localization( Identifier.de_DE, "Hallo" )
+        ( Translation( NonEmptyList.of( de ) ) & de2 ).values shouldBe
+            NonEmptyList.of( de, de2 )
+    }
+
     it should "have a String representation" in {
         Localization( Identifier.de, "Hallo" ).toString shouldBe
             """de"Hallo""""
