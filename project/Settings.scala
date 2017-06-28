3a30802cfbdc00ec08fd3de2e312dc7b5f64b8d5
Niklas Klein <mail@taig.io>
2017-06-28 12:31:37 +0200

    Resolve 2.10 build issue

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,15 +1,22 @@
 import io.taig.sbt.sonatype.SonatypeHouserulesPlugin.autoImport._
 import sbt._
 import sbt.Keys._
+import scoverage.ScoverageSbtPlugin.autoImport._
 
 object Settings {
     val Scala212 = "2.12.2"
-    
+
     val Scala211 = "2.11.11"
-    
+
     val Scala210 = "2.10.6"
-    
+
     val common = Def.settings(
+        coverageHighlighting := {
+            scalaBinaryVersion.value match {
+                case "2.10" => false
+                case _ => true
+            }
+        },
         crossScalaVersions :=
             Scala212 ::
             Scala211 ::
@@ -20,7 +27,7 @@ object Settings {
         organization := "io.taig",
         scalaVersion := Scala212
     )
-    
+
     val noPublish = Def.settings(
         publish := {},
         publishArtifact := false,
