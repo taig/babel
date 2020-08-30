0e15f16087c835e385bd9bf770011d4d128f4a58
Niklas Klein <mail@taig.io>
2020-08-30 14:52:37 +0200

    Add fromJavaLocale / toJavaLocale methods

---
+++ core/.jvm/src/test/scala/io/taig/lokal/LocaleJvmTest.scala
@@ -0,0 +1,18 @@
+package io.taig.lokal
+
+import java.util.{Locale => JLocale}
+import cats.implicits._
+
+import org.scalatest.funsuite.AnyFunSuite
+
+final class LocaleJvmTest extends AnyFunSuite {
+  test("fromJavaLocale / toJavaLocale should round trip") {
+    JLocale.getAvailableLocales.toList
+      .mapFilter(locale => Locale.fromJavaLocale(locale).tupleLeft(locale))
+      .foreach {
+        case (jLocale, locale) =>
+          assert(jLocale.getLanguage == locale.toJavaLocale.getLanguage)
+          assert(jLocale.getCountry == locale.toJavaLocale.getCountry)
+      }
+  }
+}
--- core/src/main/scala/io/taig/lokal/Locale.scala
+++ core/src/main/scala/io/taig/lokal/Locale.scala
@@ -2,11 +2,14 @@ package io.taig.lokal
 
 import cats._
 import cats.implicits._
+import java.util.{Locale => JLocale}
 
 final case class Locale(language: Language, country: Option[Country]) {
   def hasCountry: Boolean = country.isDefined
 
   def withoutCountry: Locale = Locale(language)
+
+  def toJavaLocale: JLocale = new JLocale(language.value, country.map(_.value).getOrElse(""))
 }
 
 object Locale {
@@ -14,6 +17,15 @@ object Locale {
 
   def apply(language: Language, country: Country): Locale =
     Locale(language, country.some)
+
+  def fromJavaLocale(locale: JLocale): Option[Locale] =
+    Option(locale.getLanguage)
+      .filter(_.nonEmpty)
+      .map(Language.apply)
+      .map { language =>
+        val country = Option(locale.getCountry).filter(_.nonEmpty).map(Country.apply)
+        Locale(language, country)
+      }
 
   implicit val eq: Eq[Locale] = (x, y) => x.language === y.language && x.country === y.country
 
