56cc5830a6a668ca44912c62aa355a0b8ec7413d
Niklas Klein <mail@taig.io>
2020-12-17 00:28:00 +0100

    Fix tests

--- modules/tests/shared/src/test/scala/io/taig/babel/TextTest.scala
+++ modules/tests/shared/src/test/scala/io/taig/babel/TextTest.scala
@@ -5,11 +5,11 @@ import munit.FunSuite
 final class TextTest extends FunSuite {
   test("apply prefers matching Quantities") {
     val text = Text("foo", quantities = Map(Quantity.One -> "bar"))
-    assertEquals(obtained = text.apply(Quantity.One), expected = "bar")
+    assertEquals(obtained = text(quantity = 1), expected = "bar")
   }
 
   test("apply uses fallback when no Quantity matches") {
     val text = Text("foo", quantities = Map(Quantity.One -> "bar"))
-    assertEquals(obtained = text.apply(Quantity.Zero), expected = "foo")
+    assertEquals(obtained = text(quantity = 0), expected = "foo")
   }
 }
