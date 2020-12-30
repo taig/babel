c16597024165eb2175eacbb0b4c7f9517eb6ab63
Niklas Klein <mail@taig.io>
2020-12-30 20:19:00 +0100

    Code style

--- modules/core/src/main/scala/io/taig/babel/Text.scala
+++ modules/core/src/main/scala/io/taig/babel/Text.scala
@@ -3,9 +3,8 @@ package io.taig.babel
 final case class Text(default: String, quantities: Map[Quantity, String]) {
   def raw(quantity: Int): String = quantities.getOrElse(Quantity(quantity), default)
 
-  def apply(quantity: Int, arguments: Seq[Any])(implicit formatter: Formatter): String = {
+  def apply(quantity: Int, arguments: Seq[Any])(implicit formatter: Formatter): String =
     if (arguments.isEmpty) raw(quantity) else formatter.format(raw(quantity), arguments)
-  }
 
   def apply(arguments: Seq[Any])(implicit formatter: Formatter): String = apply(quantity = 1, arguments)
 
