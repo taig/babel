9441ff1096170e1d32d773f4416c0db6085f1c84
Niklas Klein <mail@taig.io>
2021-01-26 01:14:05 +0100

    Add Dictionary.of

--- modules/core/src/main/scala/net/slozzer/babel/Dictionary.scala
+++ modules/core/src/main/scala/net/slozzer/babel/Dictionary.scala
@@ -4,3 +4,8 @@ final case class Dictionary[A](translations: Translations[A], fallback: A) {
   def apply(locale: Locale): A =
     translations.get(locale).orElse(translations.get(locale.withoutCountry)).getOrElse(fallback)
 }
+
+object Dictionary {
+  def of[A](fallback: A, translations: (Locale, A)*): Dictionary[A] =
+    Dictionary(Translations.from(translations), fallback)
+}
