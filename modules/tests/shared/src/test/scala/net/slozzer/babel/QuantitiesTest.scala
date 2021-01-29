1786613e6df9e74734533e9bb6cf4b5a1952371d
Niklas Klein <mail@taig.io>
2021-01-29 17:15:13 +0100

    Test quantity ranges

--- modules/tests/shared/src/test/scala/net/slozzer/babel/QuantitiesTest.scala
+++ modules/tests/shared/src/test/scala/net/slozzer/babel/QuantitiesTest.scala
@@ -3,7 +3,7 @@ package net.slozzer.babel
 import munit.FunSuite
 
 final class QuantitiesTest extends FunSuite {
-  test("apply prefers matching Quantities") {
+  test("apply prefers matching Quantity.Exact") {
     val text = Quantities.of("foo", Quantities.Element(Quantity.One, "bar"))
     assertEquals(obtained = text(quantity = 1), expected = "bar")
   }
@@ -11,5 +11,14 @@ final class QuantitiesTest extends FunSuite {
   test("apply uses fallback when no Quantity matches") {
     val text = Quantities.of("foo", Quantities.Element(Quantity.One, "bar"))
     assertEquals(obtained = text(quantity = 0), expected = "foo")
+  }
+
+  test("apply handles ranges") {
+    val text = Quantities.of("foo", Quantities.Element(Quantity.unsafeRange(10, 13), "bar"))
+    assertEquals(obtained = text(quantity = 9), expected = "foo")
+    assertEquals(obtained = text(quantity = 10), expected = "bar")
+    assertEquals(obtained = text(quantity = 11), expected = "bar")
+    assertEquals(obtained = text(quantity = 13), expected = "bar")
+    assertEquals(obtained = text(quantity = 14), expected = "foo")
   }
 }
