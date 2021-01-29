500ef097d6cda036c0223043fd31a21bc1de5135
Niklas Klein <mail@taig.io>
2021-01-29 16:53:39 +0100

    Quantities range support

--- modules/core/src/main/scala/net/slozzer/babel/Quantity.scala
+++ modules/core/src/main/scala/net/slozzer/babel/Quantity.scala
@@ -1,9 +1,40 @@
 package net.slozzer.babel
 
-final case class Quantity(value: Int) extends AnyVal
+sealed abstract class Quantity extends Product with Serializable {
+  def matches(quantity: Int): Boolean = this match {
+    case Quantity.Exact(value)      => value == quantity
+    case Quantity.Range(start, end) => quantity >= start && quantity <= end
+  }
+
+  final override def toString: String = this match {
+    case Quantity.Exact(value)      => s"$value"
+    case Quantity.Range(start, end) => s"$start-$end"
+  }
+}
 
 object Quantity {
-  val Zero: Quantity = Quantity(0)
+  final case class Exact private (value: Int) extends Quantity
 
-  val One: Quantity = Quantity(1)
+  final case class Range private (start: Int, end: Int) extends Quantity
+
+  val Zero: Quantity = exact(0)
+
+  val One: Quantity = exact(1)
+
+  def exact(value: Int): Quantity = Exact(value)
+
+  def unsafeRange(start: Int, end: Int): Quantity = Range(start, end)
+
+  def range(start: Int, end: Int): Option[Quantity] = Option.when(end >= start)(unsafeRange(start, end))
+
+  def parse(value: String): Either[String, Quantity] = value.split("-", 2) match {
+    case Array(value) => value.toIntOption.toRight(s"Not a valid integer: $value").map(exact)
+    case Array(start, end) =>
+      for {
+        start <- start.toIntOption.toRight(s"Not a valid integer: $start")
+        end <- end.toIntOption.toRight(s"Not a valid integer: $end")
+        range <- range(start, end).toRight("Invalid range values")
+      } yield range
+    case _ => Left("Invalid range")
+  }
 }
