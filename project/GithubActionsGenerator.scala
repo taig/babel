441d38ec03ba9cab33c0bd5204ce7d74ecde3f5a
Niklas Klein <mail@taig.io>
2022-10-11 18:06:11 +0200

    Only deploy documentation on release

--- project/GithubActionsGenerator.scala
+++ project/GithubActionsGenerator.scala
@@ -23,10 +23,11 @@ object GithubActionsGenerator {
   }
 
   object Job {
-    def apply(name: String, javaVersion: String, steps: List[Json], needs: List[String] = Nil): Json = Json.obj(
+    def apply(name: String, javaVersion: String, steps: List[Json], needs: List[String] = Nil, condition: Option[String] = None): Json = Json.obj(
       "name" := name,
       "runs-on" := "ubuntu-latest",
       "needs" := needs,
+      "if" := condition,
       "steps" := List(
         Step.Checkout,
         Step.setupJava(javaVersion)
@@ -103,6 +104,7 @@ object GithubActionsGenerator {
         name = "Deploy documentation",
         javaVersion,
         needs = List("test", "lint", "documentation"),
+        condition = Some("startsWith(github.ref, 'refs/tags/')"),
         steps = List(
           Json.obj(
             "name" := "Download artifact",
