7d1e4c2ac3051b1c6538b1d11ec5d8ac9c649cf9
Niklas Klein <mail@taig.io>
2026-04-28 11:02:15 +0200

    scalafmt

--- modules/tests/shared/src/test/scala/io/taig/babel/StringFormatTest.scala
+++ modules/tests/shared/src/test/scala/io/taig/babel/StringFormatTest.scala
@@ -41,31 +41,31 @@ final class StringFormatTest extends FunSuite {
 
   test("decoder with 5+ placeholders preserves segment order") {
     val format = Decoder[StringFormat6].decode(
-        Babel.Value("a {0} b {1} c {2} d {3} e {4} f {5} g")
+      Babel.Value("a {0} b {1} c {2} d {3} e {4} f {5} g")
     )
     assertEquals(
-        obtained = format.map(_.apply("V0", "V1", "V2", "V3", "V4", "V5")),
-        expected = Right("a V0 b V1 c V2 d V3 e V4 f V5 g")
+      obtained = format.map(_.apply("V0", "V1", "V2", "V3", "V4", "V5")),
+      expected = Right("a V0 b V1 c V2 d V3 e V4 f V5 g")
     )
   }
 
   test("decoder with 5+ placeholders out of order") {
     val format = Decoder[StringFormat6].decode(
-        Babel.Value("{5} a {3} b {1} c {4} d {0} e {2} f")
+      Babel.Value("{5} a {3} b {1} c {4} d {0} e {2} f")
     )
     assertEquals(
-        obtained = format.map(_.apply("V0", "V1", "V2", "V3", "V4", "V5")),
-        expected = Right("V5 a V3 b V1 c V4 d V0 e V2 f")
+      obtained = format.map(_.apply("V0", "V1", "V2", "V3", "V4", "V5")),
+      expected = Right("V5 a V3 b V1 c V4 d V0 e V2 f")
     )
   }
 
   test("decoder with 8 placeholders") {
     val format = Decoder[StringFormat8].decode(
-        Babel.Value("{0}-{1}-{2}-{3}-{4}-{5}-{6}-{7}")
+      Babel.Value("{0}-{1}-{2}-{3}-{4}-{5}-{6}-{7}")
     )
     assertEquals(
-        obtained = format.map(_.apply("a", "b", "c", "d", "e", "f", "g", "h")),
-        expected = Right("a-b-c-d-e-f-g-h")
+      obtained = format.map(_.apply("a", "b", "c", "d", "e", "f", "g", "h")),
+      expected = Right("a-b-c-d-e-f-g-h")
     )
   }
 }
